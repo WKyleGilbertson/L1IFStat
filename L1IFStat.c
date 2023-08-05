fb537a6f5e61c845bae07db23557ab0407a16f42
W. Kyle Gilbertson <wkyleg@gmail.com>
2023-08-05 17:57:08 -0600

    More function consolidation

--- L1IFStat.c
+++ L1IFStat.c
@@ -18,15 +18,15 @@ bool testBadCommand(FT_HANDLE ftH, BYTE cmd)
   bool retVal = false;
   FT_STATUS ftS;
   DWORD BTS = 0;  // Bytes to send
+  DWORD BTR = 0;  // Bytes to read
   DWORD NBS = 0;  // Number of bytes sent
   DWORD NBR = 0;  // Number of bytes read
-  DWORD BTR = 0;  // Bytes to read
   BYTE oBuff[64];
   BYTE iBuff[64];
   BYTE idx = 0;
 
   BTS = 0;
-  oBuff[BTS++] = 0x84; // Enable loopback
+  oBuff[BTS++] = 0x84; // Enable loopback AN 108 3.7
   ftS = FT_Write(ftH, oBuff, BTS, &NBS);
   NBS = 0;
   ftS = FT_GetQueueStatus(ftH, &NBR);
@@ -68,7 +68,7 @@ bool testBadCommand(FT_HANDLE ftH, BYTE cmd)
   }
   // Disable internal loop-back
   BTS = 0;
-  oBuff[BTS++] = 0x85; // Disable loopback
+  oBuff[BTS++] = 0x85; // Disable loopback AN 108 3.7
   ftS = FT_Write(ftH, oBuff, BTS, &NBS);
   BTS = 0;
   ftS = FT_GetQueueStatus(ftH, &NBR);
@@ -82,8 +82,22 @@ bool testBadCommand(FT_HANDLE ftH, BYTE cmd)
   else {
 //    fprintf(stderr, "Loopback Disabled\n");
   }
-/* End of testing bad command part, 5.3.1*/
+/* End of testing bad command AN 135 5.3.1*/
 return retVal;
+}
+
+void displayDevInfo(FT_DEVICE_LIST_INFO_NODE * dInfo, DWORD numD) {
+  WORD i;
+  for (i = 0; i<numD; i++) {
+    printf("Dev %d:\n", i);
+    printf("  Flags=0x%x\n", dInfo[i].Flags);
+    printf("  Type=0x%x\n", dInfo[i].Type);
+    printf("  ID=0x%x\n", dInfo[i].ID);
+    printf("  LocId=0x%x\n", dInfo[i].LocId);
+    printf("  SerialNumber=%s\n", dInfo[i].SerialNumber);
+    printf("  Description=%s\n", dInfo[i].Description);
+    printf("  ftHandle=0x%x\n", (DWORD) dInfo[i].ftHandle);
+  }
 }
 
 int main()
@@ -116,16 +130,7 @@ int main()
 // get the device information list
     ftStatus = FT_GetDeviceInfoList(devInfo, &numDevs); // AN_135 4.1 Step 2
    if (ftStatus == FT_OK) {
-   for (i = 0; i < numDevs; i++) {
-    printf("Dev %d:\n", i);
-    printf("  Flags=0x%x\n", devInfo[i].Flags);
-    printf("  Type=0x%x\n", devInfo[i].Type);
-    printf("  ID=0x%x\n", devInfo[i].ID);
-    printf("  LocId=0x%x\n", devInfo[i].LocId);
-    printf("  SerialNumber=%s\n", devInfo[i].SerialNumber);
-    printf("  Description=%s\n", devInfo[i].Description);
-    printf("  ftHandle=0x%x\n", (DWORD) devInfo[i].ftHandle);
-   }
+   displayDevInfo(devInfo, numDevs);
   }
  }
   else {
@@ -170,140 +175,44 @@ int main()
   printf("Bad command 0xAB was echoed? %s\n",
    (testBadCommand(ftdiHandle, 0xAB) == true) ? "yes" : "no" );
 
-  /*
-  //////////////////////////////////////////////////////////////////
-	// Synchronize the MPSSE interface by sending bad command ¡®0xAA¡¯
-	//////////////////////////////////////////////////////////////////
-  // Enable internal loop-back AN 135 Section 5.3.1
-  dwNumBytesToSend = 0;
-  OutputBuffer[dwNumBytesToSend++] = 0x84; // Enable loopback
-  ftStatus = FT_Write(ftdiHandle, OutputBuffer, dwNumBytesToSend, &dwNumBytesSent);
-  dwNumBytesToSend = 0;
-  ftStatus = FT_GetQueueStatus(ftdiHandle, &dwNumBytesRead);
-  if (dwNumBytesRead != 0) {
-    printf("Error - MPSSE receive buffer should be empty\n", ftStatus);
-    FT_SetBitMode(ftdiHandle, 0x00, 0x00);
-    FT_Close(ftdiHandle);
-    return 1;
-  }
-  else {
-    printf("Loopback Enabled\n");
-  }
-
-	dwNumBytesToSend = 0;
-	OutputBuffer[dwNumBytesToSend++] = 0xAA;		//Add BAD command ¡®0xAA¡¯
-	ftStatus = FT_Write(ftdiHandle, OutputBuffer, dwNumBytesToSend, &dwNumBytesSent);	// Send off the BAD commands
-	printf("Bytes Sent: %d\n", dwNumBytesSent);
-	dwNumBytesToSend = 0;			//Clear output buffer
-	do{
-		ftStatus = FT_GetQueueStatus(ftdiHandle, &dwNumBytesToRead);	 // Get the number of bytes in the device input buffer
-	} while ((dwNumBytesToRead == 0) && (ftStatus == FT_OK));   	//or Timeout
-
-	ftStatus = FT_Read(ftdiHandle, InputBuffer, dwNumBytesToRead, &dwNumBytesRead);  //Read out the data from input buffer
-	for (dwCount = 0; dwCount < (dwNumBytesRead - 1); dwCount++)	//Check if Bad command and echo command received
-	{
-		if ((InputBuffer[dwCount] == 0xFA) && (InputBuffer[dwCount + 1] == 0xAA))
-		{
-			bCommandEchod = true;
-			break;
-		}
-	}
-	if (bCommandEchod == false)
-	{
-		printf("fail to synchronize MPSSE with command '0xAA' \n");
-		return false; //Error, can¡¯t receive echo command , fail to synchronize MPSSE interface;
-	}
-  else {
-    printf("Bad command 0xAA was Echoed\n");
-    bCommandEchod = false;
-  }
-
-	//////////////////////////////////////////////////////////////////
-	// Synchronize the MPSSE interface by sending bad command ¡®0xAB¡¯
-	//////////////////////////////////////////////////////////////////
-	dwNumBytesToSend = 0;			//Clear output buffer
-	OutputBuffer[dwNumBytesToSend++] = 0xAB;	//Send BAD command ¡®0xAB¡¯
-	ftStatus = FT_Write(ftdiHandle, OutputBuffer, dwNumBytesToSend, &dwNumBytesSent);	// Send off the BAD commands
-  printf("Bytes Sent: %d\n", dwNumBytesSent);
-	dwNumBytesToSend = 0;			//Clear output buffer
-	do{
-		ftStatus = FT_GetQueueStatus(ftdiHandle, &dwNumInputBuffer);	//Get the number of bytes in the device input buffer
-	} while ((dwNumInputBuffer == 0) && (ftStatus == FT_OK));   //or Timeout
-	bCommandEchod = false;
-	ftStatus = FT_Read(ftdiHandle, InputBuffer, dwNumInputBuffer, &dwNumBytesRead);  //Read out the data from input buffer
-	for (dwCount = 0; dwCount < (dwNumBytesRead - 1); dwCount++)	//Check if Bad command and echo command received
-	{
-		if ((InputBuffer[dwCount] == 0xFA) && (InputBuffer[dwCount + 1] == 0xAB))
-		{
-			bCommandEchod = true;
-			break;
-		}
-	}
-	if (bCommandEchod == false)
-	{
-		printf("fail to synchronize MPSSE with command '0xAB' \n");
-		return false;
-		//Error, can't receive echo command , fail to synchronize MPSSE interface;
-	}
-  else {
-      printf("Bad command 0xAB was echoed\n");
-      bCommandEchod = false;
-  }
-  // Disable internal loop-back
-  dwNumBytesToSend = 0;
-  OutputBuffer[dwNumBytesToSend++] = 0x85; // Disable loopback
-  ftStatus = FT_Write(ftdiHandle, OutputBuffer, dwNumBytesToSend, &dwNumBytesSent);
-  dwNumBytesToSend = 0;
-  ftStatus = FT_GetQueueStatus(ftdiHandle, &dwNumBytesRead);
-  if (dwNumBytesRead != 0) {
-    printf("Error - MPSSE receive buffer should be empty\n", ftStatus);
-    FT_SetBitMode(ftdiHandle, 0x00, 0x00);
-    FT_Close(ftdiHandle);
-    return 1;
-  }
-  else {
-    printf("Loopback Disabled\n");
-  }
-*/
-/* End of testing bad command part, 5.3.1*/
+/* End of testing bad command part, AN 135 5.3.1*/
 
 /* Configure SPI here. Do I need to? */
 
 /* Now READ the GPIO to See if Antenna is attached*/
+/* Write function bool testAntennaConnected(ftdiHandle) */
 // Change scope trigger to channel 4 (TMS/CS) falling edge
 dwNumBytesToSend = 0;
-OutputBuffer[dwNumBytesToSend++] = 0x81;
-// Get data bits - returns state of pins,
-// either input or output
+OutputBuffer[dwNumBytesToSend++] = 0x81; // AN 108 3.6
+// Get data bits - returns state of pins, either input or output
 // on low byte of MPSSE
 ftStatus = FT_Write(ftdiHandle, OutputBuffer, dwNumBytesToSend, &dwNumBytesSent);
 // Read the low GPIO byte
 dwNumBytesToSend = 0; // Reset output buffer pointer
-Sleep(2); // Wait for data to be transmitted and status
-// to be returned by the device driver
-// - see latency timer above
+Sleep(2); // Wait for data to be transmitted and status to be returned 
+// by the device driver - see latency timer above
 // Check the receive buffer - there should be one byte
 ftStatus = FT_GetQueueStatus(ftdiHandle, &dwNumBytesToRead);
-// Get the number of bytes in the
-// FT2232H receive buffer
+// Get the number of bytes in the FT2232H receive buffer
 ftStatus |= FT_Read(ftdiHandle, &InputBuffer, dwNumBytesToRead, &dwNumBytesRead);
 if ((ftStatus != FT_OK) & (dwNumBytesToRead != 1))
 {
 printf("Error - GPIO cannot be read\n");
-FT_SetBitMode(ftdiHandle, 0x0, 0x00);
-// Reset the port to disable MPSSE
-FT_Close(ftdiHandle); // Close the USB port
-return 1; // Exit with error
+FT_SetBitMode(ftdiHandle, 0x0, 0x00); // Reset the port to disable MPSSE
+FT_Close(ftdiHandle);                 // Close the USB port
+return 1;                             // Exit with error
 }
+else {
 printf("The GPIO low-byte = 0x%X\n", InputBuffer[0]);
 antennaConnected = ((InputBuffer[0] & 0x20) >> 5) == 1 ? true : false; 
 printf("Antenna connected? %s\n", (antennaConnected) == true ? "yes" : "no");
-// The inpute buffer only contains one
-// valid byte at location 0
-printf("Press <Enter> to continue\n");
-getchar(); // wait for a carriage return
+// The input buffer only contains one valid byte at location 0
 // Modify the GPIO data (TMS/CS only) and write it back
 /* End GPIO Read*/
+}
+
+printf("Press <Enter> to continue\n");
+getchar(); // wait for a carriage return
 
 FT_SetBitMode(ftdiHandle, 0x00, 0x00);  // Reset MPSSE
 FT_Close(ftdiHandle);                   // Close Port
